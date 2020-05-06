ae998175f0718ddb3f861e303f595474c013b13b
Phillip Jones <djones.phillip@gmail.com>
2020-05-05 23:23:15 -0700

    Added sentry to test-site

--- test-site/src/App.js
+++ test-site/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css'
 
+function throwException() {
+  console.log(1/0);
+}
+
 export default function App() {
   return (
     <div className="App">
       <h1>Test site</h1>
+      <button onClick={throwException}>Break the world</button>
     </div>
   )
 }
--- test-site/src/index.js
+++ test-site/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/browser';
+
 import App from './App'
 import './index.css'
+
+Sentry.init({dsn: "https://f0ebf5ae270842abbe2dc1b441bed3d9@o328915.ingest.sentry.io/5225138"});
 
 ReactDOM.render(
   <React.StrictMode>
