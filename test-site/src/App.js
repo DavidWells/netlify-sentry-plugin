27bb9df7977c071fdec25befe4725b56355821a0
Phillip Jones <djones.phillip@gmail.com>
2020-05-07 15:07:05 -0700

    Add release tag in sdk init

--- test-site/src/App.js
+++ test-site/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './App.css'
+const { promisify, inspect } = require('util')
 
 function throwException() {
+  console.log(inspect(process.env, { showHidden: false, depth: null }))
   throw new Error('Exception on purpose')
 }
 
--- test-site/src/index.js
+++ test-site/src/index.js
@@ -5,7 +5,10 @@ import * as Sentry from '@sentry/browser';
 import App from './App'
 import './index.css'
 
-Sentry.init({dsn: "https://f0ebf5ae270842abbe2dc1b441bed3d9@o328915.ingest.sentry.io/5225138"});
+Sentry.init({
+  dsn: "https://f0ebf5ae270842abbe2dc1b441bed3d9@o328915.ingest.sentry.io/5225138",
+  release: process.env.COMMIT_REF,
+});
 
 ReactDOM.render(
   <React.StrictMode>
