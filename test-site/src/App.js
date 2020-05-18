56dacf4b687649408fe6775d39ed0a45c74e6378
Phillip Jones <djones.phillip@gmail.com>
2020-05-18 10:09:12 -0700

    wip - test - skipsourcemaps

--- test-site/src/App.js
+++ test-site/src/App.js
@@ -10,7 +10,7 @@ function throwException() {
 export default function App() {
   return (
     <div className="App">
-      <h1>Test site .......</h1>
+      <h1>Test site ........</h1>
       <button onClick={throwException}>Break the world</button>
     </div>
   )
