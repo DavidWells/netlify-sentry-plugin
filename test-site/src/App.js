9747f0fab3d242fee051c1a9110c4c857d6bb384
Phillip Jones <djones.phillip@gmail.com>
2020-06-05 10:50:53 -0700

    Test commit

--- test-site/src/App.js
+++ test-site/src/App.js
@@ -3,15 +3,14 @@ import './App.css'
 const { promisify, inspect } = require('util')
 
 function throwException() {
-  console.log(inspect(process.env, { showHidden: false, depth: null }))
   throw new Error('Exception on purpose')
 }
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Test site .........a.test...test..d.............1.0.3mixedvars..fromnpm</h1>
-      <button onClick={throwException}>Break the world</button>
+      <h1>Example site</h1>
+      <button onClick={throwException}>Throw exception</button>
     </div>
   )
 }
